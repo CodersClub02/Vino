4b4a095096b543e38e5801733921b6cb7144d8c7
Hanane Boukhalfa <e2195837@cmaisonneuve.qc.ca>
2023-04-19 20:58:49 -0400

    Ajouter du style en survolent le bouton

--- vue/src/components/Inscrire.vue
+++ vue/src/components/Inscrire.vue
@@ -85,7 +85,7 @@ const creerCompte = async () => {
 
                         <div>
                             <button type="submit"
-                                class="flex w-full justify-center rounded-md bg-rose-800 px-3 p-3 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-800">S'inscrire</button>
+                                class="flex w-full justify-center rounded-md bg-rose-800 px-3 p-3 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-rose-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-800">S'inscrire</button>
 
                         </div>
                     </form>
