5f0306279cf4c28d24be5d37532d5ac53c1cf7d5
Hanane Boukhalfa <e2195837@cmaisonneuve.qc.ca>
2023-04-21 09:19:08 -0400

    Ajouter la vue pour l'ajout d'une bouteille

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -7,6 +7,7 @@ const routes = [
     { path: "/connecter", name: "Connecter", component: () => import("../components/Connecter.vue") },
     { path: "/creer-compte", name: "CreerCompte", component: () => import("../components/Inscrire.vue") },
     { path: "/profil", name: "Profil", component: () => import("../components/Profil.vue") },
+    { path: "/ajouter-bouteille", name: "AjouterBouteille", component: () => import("../components/AjouterBouteille.vue") },
 ]
 
 const router = createRouter({
