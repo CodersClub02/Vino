32840d86a8e124a8760aced941d6fa9cdfd6702a
sadektouati <bilal2dellys@hotmail.com>
2023-04-21 05:42:58 -0400

    initialiser tous ce qui est base des données: migrations, models, factories...

---
+++ api/database/migrations/2023_04_20_233722_create_bouteilles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Type;
+use App\Models\Pays;
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bouteilles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->foreignIdFor(Type::class);
+            $table->foreignIdFor(Pays::class);
+
+            $table->string('nom', 200);
+            $table->string('code_saq', 50)->nullable();
+            $table->string('description_saq', 500)->nullable();
+            $table->string('format', 20)->nullable();
+            $table->decimal('prix_saq')->nullable();
+            $table->string('url_saq', 1000)->nullable();
+            $table->string('url_image_saq', 1000)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bouteilles');
+    }
+};
