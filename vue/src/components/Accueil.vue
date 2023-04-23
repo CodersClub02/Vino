f1561c761178bd38ac2b7193e9f913635303b13b
Hanane Boukhalfa <e2195837@cmaisonneuve.qc.ca>
2023-04-23 16:04:58 -0400

    Changer de couleur du titre

--- vue/src/components/Accueil.vue
+++ vue/src/components/Accueil.vue
@@ -31,39 +31,43 @@ const form = ref({
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8" v-else>
       <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 class="text-rose-800 mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Entrez à votre compte</h2>
+          <h2 class="text-rose-800 mt-10 text-center text-2xl font-bold leading-9 tracking-tight">Entrez à votre compte
+          </h2>
         </div>
 
-              <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form @submit.prevent="authStore.connecter(form)" class="space-y-6">
-                  <div>
-                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Courriel</label>
-                    <div class="mt-2">
-                      <input id="email" name="courriel" v-model="form.courriel" type="email" autocomplete="email" class="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-rose-800 sm:text-sm sm:leading-6" />
-                    </div>
-                    <div v-if="authStore.erreurs.email" class="mt-2" >
-                      <span class="text-red-400 text-sm m-2 p-2">
-                        {{ authStore.erreurs.email[0] }}
-                      </span>
-                    </div>
-                  </div>
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form @submit.prevent="authStore.connecter(form)" class="space-y-6">
+            <div>
+              <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Courriel</label>
+              <div class="mt-2">
+                <input id="email" name="courriel" v-model="form.courriel" type="email" autocomplete="email"
+                  class="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-rose-800 sm:text-sm sm:leading-6" />
+              </div>
+              <div v-if="authStore.erreurs.email" class="mt-2">
+                <span class="text-red-400 text-sm m-2 p-2">
+                  {{ authStore.erreurs.email[0] }}
+                </span>
+              </div>
+            </div>
 
-                  <div>
-                    <div class="flex items-center justify-between">
-                      <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Mot de passe</label>
-                      <div class="text-sm">
-                        <a href="#" class="font-semibold text-rose-800 hover:text-red-500">Mot de passe oublié?</a>
-                      </div>
-                    </div>
-                    <div class="mt-2">
-                      <input id="password" name="mot_de_passe" v-model="form.mot_de_passe" type="password" autocomplete="current-password" class="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-rose-800 sm:text-sm sm:leading-6" />
-                    </div>
-                    <div v-if="authStore.erreurs.password" class="mt-2" >
-                      <span class="text-red-400 text-sm m-2 p-2">
-                        {{ authStore.erreurs.password[0] }}
-                      </span>
-                    </div>
-                  </div>
+            <div>
+              <div class="flex items-center justify-between">
+                <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Mot de passe</label>
+                <div class="text-sm">
+                  <a href="#" class="font-semibold text-rose-800 hover:text-red-500">Mot de passe oublié?</a>
+                </div>
+              </div>
+              <div class="mt-2">
+                <input id="password" name="mot_de_passe" v-model="form.mot_de_passe" type="password"
+                  autocomplete="current-password"
+                  class="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-rose-800 sm:text-sm sm:leading-6" />
+              </div>
+              <div v-if="authStore.erreurs.password" class="mt-2">
+                <span class="text-red-400 text-sm m-2 p-2">
+                  {{ authStore.erreurs.password[0] }}
+                </span>
+              </div>
+            </div>
 
             <div>
               <button type="submit"
@@ -79,8 +83,8 @@ const form = ref({
 
           </p>
         </div>
+      </div>
     </div>
-  </div>
 
 
 
@@ -88,46 +92,56 @@ const form = ref({
 
   <div class="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
     <div class="absolute inset-0 -z-10 overflow-hidden">
-      <svg class="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]" aria-hidden="true">
+      <svg
+        class="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
+        aria-hidden="true">
         <defs>
-          <pattern id="e813992c-7d03-4cc4-a2bd-151760b470a0" width="200" height="200" x="50%" y="-1" patternUnits="userSpaceOnUse">
+          <pattern id="e813992c-7d03-4cc4-a2bd-151760b470a0" width="200" height="200" x="50%" y="-1"
+            patternUnits="userSpaceOnUse">
             <path d="M100 200V.5M.5 .5H200" fill="none" />
           </pattern>
         </defs>
         <svg x="50%" y="-1" class="overflow-visible fill-gray-50">
-          <path d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z" stroke-width="0" />
+          <path d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
+            stroke-width="0" />
         </svg>
         <rect width="100%" height="100%" stroke-width="0" fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" />
       </svg>
     </div>
-    <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
-      <div class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+    <div
+      class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+      <div
+        class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
         <div class="lg:pr-4">
           <div class="lg:max-w-lg">
             <p class="text-base font-semibold leading-7 text-indigo-600">Deploy faster</p>
             <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</h1>
-            <p class="mt-6 text-xl leading-8 text-gray-700">Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas.</p>
+            <p class="mt-6 text-xl leading-8 text-gray-700">Aliquet nec orci mattis amet quisque ullamcorper neque, nibh
+              sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas.</p>
           </div>
         </div>
       </div>
-      <div class="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+      <div
+        class="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
         <div class="lg:pr-4">
           <div class="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
             <ul role="list" class="mt-8 space-y-8 text-gray-600">
               <li class="flex gap-x-3">
-                <span><strong class="font-semibold text-gray-900">Push to deploy.</strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</span>
+                <span><strong class="font-semibold text-gray-900">Push to deploy.</strong> Lorem ipsum, dolor sit amet
+                  consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate
+                  blanditiis ratione.</span>
               </li>
               <li class="flex gap-x-3">
-                <span><strong class="font-semibold text-gray-900">SSL certificates.</strong> Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.</span>
-              </li>
-              <li class="flex gap-x-3">
-                <span><strong class="font-semibold text-gray-900">Database backups.</strong> Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</span>
-              </li>
-            </ul>
-          </div>
+              <span><strong class="font-semibold text-gray-900">SSL certificates.</strong> Anim aute id magna aliqua ad
+                ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.</span>
+            </li>
+            <li class="flex gap-x-3">
+              <span><strong class="font-semibold text-gray-900">Database backups.</strong> Ac tincidunt sapien vehicula
+                erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</span>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
   </div>
-
-</template>
+</div></template>
