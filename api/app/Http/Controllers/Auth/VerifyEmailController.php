2b0ea8895a1774c8d68b1a1197ffd08a0d1a8bcd
Hanane Boukhalfa <e2195837@cmaisonneuve.qc.ca>
2023-04-28 14:59:06 -0400

    Ajouter des commentaires

--- api/app/Http/Controllers/Auth/VerifyEmailController.php
+++ api/app/Http/Controllers/Auth/VerifyEmailController.php
@@ -11,7 +11,7 @@ use Illuminate\Http\RedirectResponse;
 class VerifyEmailController extends Controller
 {
     /**
-     * Mark the authenticated user's email address as verified.
+     * Marquer l'email d'un usager authentifié comme vérifié .
      */
     public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
