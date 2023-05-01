e15ae3521d6d35c7ac4522d86176be0cf25b6305
sadektouati <bilal2dellys@hotmail.com>
2023-05-01 09:14:50 -0400

    ameliorer l'architecture d'information sur la carte bouteille

--- vue/src/components/Bouteille.vue
+++ vue/src/components/Bouteille.vue
@@ -10,36 +10,51 @@ defineProps({
 
 <template>
 
-<article class="flex gap-2 p-3 bg-yellow-100">
-    <a class="w-40" :href="bouteille.url_saq">
+<article class="flex flex-col gap-2 p-3 bg-yellow-100">
+    
+    <a class="w-40 relative" :href="bouteille.url_saq">
+        <sup class="absolute z-10 top-1 bg-white flex p-2">{{ bouteille.code_saq }}</sup>
         <img :src="bouteille.url_image_saq" class="w-full"/>
-        <sup>{{ bouteille.code_saq }}</sup>
     </a>
     
     <section class="flex flex-col gap-2">
-        <div>
-            <header class="flex gap-2 items-baseline">
-                <h1 class="text-lg font-semibold">{{ bouteille.nom }}</h1>
-                {{ bouteille.format }}
-            </header>
+        <header class="flex flex-col gap-2 items-baseline">
+            <h1 class="text-lg font-semibold">{{ bouteille.nom }}</h1>
             <h2>
-                <span class="font-bold text-red-600">{{ bouteille.prix_saq }}$</span>
+                <span class="font-bold text-red-600">{{ bouteille.prix_paye }}$ {{ bouteille.prix_saq }}$</span>
                 |
                 {{ bouteille.type.nom }}
                 |
+                {{ bouteille.format }}
+                |
                 {{ bouteille.pays.nom }}
             </h2>
+        </header>
+
+        <div class="flex gap-5 justify-center items-center">
+            <div class="flex gap-2 items-center bg-slate-400">
+                <span class="flex flex-col gap-3">
+                    <span class="flex justify-center items-center bg-orange-600 aspect-square w-8 text-2xl text-white cursor-pointer">+</span>
+                    <span class="flex justify-center items-center bg-orange-600 aspect-square w-8 text-2xl text-white cursor-pointer">-</span>
+                </span>
+                <div class="flex text-4xl px-10 w-40 justify-center items-center">{{ bouteille.notes }}</div>
+            </div>
+            <div class="flex gap-2 items-center bg-slate-400">
+                <div class="flex text-4xl px-10 w-40 justify-center items-center">{{ bouteille.quantite }}</div>
+                <span class="flex flex-col gap-3">
+                    <span class="flex justify-center items-center bg-orange-600 aspect-square w-8 text-2xl text-white cursor-pointer">+</span>
+                    <span class="flex justify-center items-center bg-orange-600 aspect-square w-8 text-2xl text-white cursor-pointer">-</span>
+                </span>
+            </div>
         </div>
 
-        <div>
-            <div>date_achat: {{ bouteille.date_achat }}</div>
-            <div>garder_jusqu_a: {{ bouteille.garder_jusqu_a }}</div>
-            <div>notes: {{ bouteille.notes }}</div>
-            <div>prix_paye: {{ bouteille.prix_paye }}</div>
-            <div>quantite: {{ bouteille.quantite }}</div>
-            <div>mellisme: {{ bouteille.mellisme }}</div>
-            <label class="cursor-pointer border-b-rose-300 bg-purple-400 p-1" @click="appStore.togglerFormBouteille(), formBouteille = bouteille">modifier</label>
-        </div>
+        <label class="cursor-pointer border-b-rose-300 bg-purple-400 p-1" @click="appStore.togglerFormBouteille(), formBouteille = bouteille">modifier</label>
+        
+        <div>comentaire: {{ bouteille.comentaire }}</div>
+        <div>mellisme: {{ bouteille.mellisme }}</div>
+        <div>date_achat: {{ bouteille.date_achat }}</div>
+        <div>garder_jusqu_a: {{ bouteille.garder_jusqu_a }}</div>
+
     </section>
 </article>
 </template>
