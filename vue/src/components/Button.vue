a4ea447e33b2e6747bd1d89cca256d97470135a9
Hanane Boukhalfa <e2195837@cmaisonneuve.qc.ca>
2023-05-02 17:10:52 -0400

    Modifier la largeur des boutons

--- vue/src/components/Button.vue
+++ vue/src/components/Button.vue
@@ -7,5 +7,6 @@ defineProps({
 </script>
 
 <template>
-    <button type="submit" class="flex min-w-min justify-center rounded-md bg-rose-800 px-3 p-3 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-rose-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-800">{{ texteBouton }}</button>
+    <button type="submit"
+        class=" w-full justify-center rounded-md bg-rose-800 px-3 p-3 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-rose-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-800">{{ texteBouton }}</button>
 </template>
