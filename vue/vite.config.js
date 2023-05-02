630eca60c370fda2d25fb390f78cacfeaae6d615
Hanane Boukhalfa <e2195837@cmaisonneuve.qc.ca>
2023-05-02 17:22:38 -0400

    Exclure @fortawesome/vue-fontawesome

--- vue/vite.config.js
+++ vue/vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    exclude: ['@fortawesome/vue-fontawesome']
+  },
   plugins: [vue()],
   server: {
     port: 3000,
