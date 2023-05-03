3caee221d5e79305f6267e91ad8cfa763a7d5c7d
Hanane Boukhalfa <e2195837@cmaisonneuve.qc.ca>
2023-05-03 16:45:51 -0400

    Ajouter des icônes pour pour modifier, supprimer ou visionner une bouteille

--- vue/src/components/Bouteille.vue
+++ vue/src/components/Bouteille.vue
@@ -3,8 +3,6 @@ import { watch } from "vue";
 import { useAppStore } from '../stores/app'
 const appStore = useAppStore()
 
-import { defineProps } from 'vue';
-
 const props = defineProps({
     bouteille: {
         type: Object,
@@ -19,41 +17,52 @@ watch(props.bouteille, (currentState, prevState) => {
 </script>
 
 <template>
+    <article @click="appStore.togglerFormCellier(), form.nom = form.nomEnCours"
+        class="relative flex flex-col gap-2 p-3 shadow-md bg-white">
 
-<article class="flex flex-col gap-2 p-3 shadow-md bg-white">
-    
-    <a class="relative w-full" :href="bouteille.url_saq">
-        <img :src="bouteille.url_image_saq" class="h-48 m-auto"/>
-    </a>
-    
-    <section class="flex flex-col gap-2">
-        <header class="flex flex-col gap-2 items-baseline">
-            <h1 class="w-full text-lg font-semibold md:truncate lg:truncate xl:truncate">{{ bouteille.nom }}</h1>
-            <h2 class="truncate">
-                <span class="font-bold text-red-600">{{ bouteille.prix_paye }}$</span>
-                |
-                {{ bouteille.type.nom }}
-                |
-                {{ bouteille.format }}
-                |
-                {{ bouteille.pays.nom }}
-            </h2>
-        </header>
+        <img :src="bouteille.url_image_saq" class="h-48 m-auto" />
 
-        <div class="w-full flex gap-2 justify-center items-center p-3">
-            <span v-for="i in 5" class="flex flex-col justify-center items-center rounded-3xl cursor-pointer w-7 h-10" @click="bouteille.notes=i">
-                <img :src="'/etoile' + ( i <= bouteille.notes ? '-solide' : '' ) + '.png'">
-            </span>
-        </div>
-        <div class="w-full flex gap-1 justify-center items-center p-3 border rounded-sm">
-            <span class="flex flex-col gap-3 justify-center items-center bg-rose-600 aspect-square text-2xl text-white rounded-3xl cursor-pointer w-10 h-10" @click="--bouteille.quantite">-</span>
+        <section class="flex flex-col gap-2">
+            <header class="flex flex-col gap-2 items-baseline">
+                <h1 class="w-full text-lg font-semibold md:truncate lg:truncate xl:truncate">{{ bouteille.nom }}</h1>
+                <h2 class="truncate">
+                    <span class="font-bold text-red-600">{{ bouteille.prix_paye }}$</span>
+                    |
+                    {{ bouteille.type.nom }}
+                    |
+                    {{ bouteille.format }}
+                    |
+                    {{ bouteille.pays.nom }}
+                </h2>
+            </header>
+
+            <div class="w-full flex gap-2 justify-center items-center p-3">
+                <span v-for="i in 5" class="flex flex-col justify-center items-center rounded-3xl cursor-pointer w-7 h-10"
+                    @click="bouteille.notes = i">
+                    <img :src="'/etoile' + (i <= bouteille.notes ? '-solide' : '') + '.png'">
+                </span>
+            </div>
+            <div class="w-full flex gap-1 justify-center items-center p-3 border rounded-sm">
+                <span
+                    class="flex flex-col gap-3 justify-center items-center bg-rose-600 aspect-square text-2xl text-white rounded-3xl cursor-pointer w-10 h-10"
+                    @click="--bouteille.quantite">-</span>
                 <div class="flex text-4xl px-10 grow justify-center items-center">{{ bouteille.quantite }}</div>
-                <span class="flex flex-col gap-3 justify-center items-center bg-rose-600 aspect-square text-2xl text-white rounded-3xl cursor-pointer w-10 h-10" @click="++bouteille.quantite">+</span>
-        </div>
+                <span
+                    class="flex flex-col gap-3 justify-center items-center bg-rose-600 aspect-square text-2xl text-white rounded-3xl cursor-pointer w-10 h-10"
+                    @click="++bouteille.quantite">+</span>
+            </div>
 
-        <label class="cursor-pointer border-b-rose-300 bg-purple-400 p-1"
-         @click="appStore.togglerFormBouteille(bouteille)">modifier</label>
 
-    </section>
-</article>
+            <div class="flex gap-5 justify-end flex-col absolute right-3 top-5">
+                <font-awesome-icon icon="fa-solid fa-trash" class="text-gray-400 cursor-pointer" @click="" />
+
+                <font-awesome-icon icon="fa-solid fa-pen-to-square" @click="" class="text-gray-400 cursor-pointer" />
+
+                <a class="" :href="bouteille.url_saq">
+                    <font-awesome-icon icon="fa-solid fa-eye" class="text-gray-400 cursor-pointer" />
+                </a>
+            </div>
+
+        </section>
+    </article>
 </template>
