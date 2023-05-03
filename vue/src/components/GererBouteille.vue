a19659acc1a9e22b4f475dc3d5fb0fea4bdf5b68
Hanane Boukhalfa <e2195837@cmaisonneuve.qc.ca>
2023-05-03 18:20:01 -0400

    Enlever de l'HTML et LE CSS inutiles pour un meilleur affichage du formulaire

--- vue/src/components/GererBouteille.vue
+++ vue/src/components/GererBouteille.vue
@@ -31,109 +31,107 @@ const tableauNotes = [{ id: 1, nom: '1 étoile' }, { id: 2, nom: '2 étoiles' },
 </script>
 
 <template>
-    <div class="mx-auto max-w-7xl sm:px-6 bg-white flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                <template v-if="appStore.bouteilleSelectione.id">Modifier bouteille</template>
-                <template v-else>Ajouter bouteille à
-                    {{ cellier.nomEnCours }}</template>
-            </h2>
-        </div>
+    <div class="mx-auto  w-full max-w-lg bg-white flex gap-10 flex-col justify-center p-6">
 
-        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form
-                @submit.prevent="(appStore.bouteilleSelectione.id ? appStore.modifierBouteille(appStore.bouteilleSelectione) : appStore.ajouterBouteille(appStore.bouteilleSelectione))"
-                class="space-y-6">
+        <h2 class="sm:mx-auto sm:w-full sm:max-w-sm text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            <template v-if="appStore.bouteilleSelectione.id">Modifier bouteille</template>
+            <template v-else>Ajouter bouteille à
+                {{ cellier.nomEnCours }}</template>
+        </h2>
 
-                <template v-if="!appStore.bouteilleSelectione.id">
-                    <div>
-                        <div class="flex items-center justify-between">
-                            <label class="text-sm leading-6 text-gray-900 font-semibold">Source bouteille</label>
-                        </div>
 
-                        <div
-                            class="mt-2 flex gap-6 w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-rose-800 sm:text-sm sm:leading-6">
-                            <label class="flex gap-2 text-sm leading-6 text-gray-900 ">
-                                <input type="radio" class="form-radio accent-rose-600" name="source" value="saq"
-                                    v-model="appStore.bouteilleSelectione.source" checked>
-                                la SAQ
-                            </label>
-                            <label class="flex gap-2 text-sm leading-6 text-gray-900 ">
-                                <input type="radio" class="form-radio accent-rose-600" name="source" value="autre"
-                                    v-model="appStore.bouteilleSelectione.source">
-                                Autre
-                            </label>
-                        </div>
+        <form
+            @submit.prevent="(appStore.bouteilleSelectione.id ? appStore.modifierBouteille(appStore.bouteilleSelectione) : appStore.ajouterBouteille(appStore.bouteilleSelectione))"
+            class="flex gap-8 flex-col mx-auto w-full ">
+
+            <template v-if="!appStore.bouteilleSelectione.id">
+                <div>
+                    <div class="flex items-center justify-between">
+                        <label class="text-sm leading-6 text-gray-900 font-semibold">Source bouteille</label>
                     </div>
 
-                    <Input list="suggestions" v-if="(appStore.bouteilleSelectione.source || 'saq') == 'saq'"
-                        v-bind:erreur="appStore.erreursBouteille.bouteille_id" v-model="appStore.bouteilleSelectione.nom"
-                        label="Code saq ou nom bouteille" type="text" autocomplete="off"
-                        @input="appStore.listeSuggestionsBouteilles($event.target.value), afficherSuggestionsBouteilles = true">
-                    <template v-slot:liste>
-
-                        <datalist id="suggestions">
-                            <option v-for="bouteille in appStore.suggestionsBouteilles">
-                                {{ bouteille.nom }}
-                            </option>
-                        </datalist>
-
-                    </template>
-
-                    </Input>
-                    <template v-if="appStore.bouteilleSelectione.source == 'autre'">
-
-                        <Input v-bind:erreur="appStore.erreursBouteille.nom" v-model="appStore.bouteilleSelectione.nom"
-                            label="Nom de bouteille" name="nom" type="text" />
-
-                        <Input v-bind:erreur="appStore.erreursBouteille.format"
-                            v-model="appStore.bouteilleSelectione.format" label="Format" name="format" type="text" />
-
-                        <Select v-model="appStore.bouteilleSelectione.type_id" :options="appStore.listeType"
-                            v-bind:erreur="appStore.erreursBouteille.type_id" label="Type" />
-
-                        <Select v-model="appStore.bouteilleSelectione.pays_id" :options="appStore.listePays"
-                            v-bind:erreur="appStore.erreursBouteille.pays_id" label="Pays" />
-                    </template>
-                </template>
-
-                <template v-if="appStore.bouteilleSelectione.id">
-
-                    <Select v-if="appStore.celliers.length > 1" v-model="appStore.bouteilleSelectione.cellier_id"
-                        :options="appStore.celliers" v-bind:erreur="appStore.erreursBouteille.cellier_id" label="Cellier" />
-
-
-                    <Select v-model="appStore.bouteilleSelectione.notes" name="notes" :options="tableauNotes"
-                        v-bind:erreur="appStore.erreursBouteille.notes" label="Notes" />
-
-                    <Textarea v-bind:erreur="appStore.erreursBouteille.commentaire"
-                        v-model="appStore.bouteilleSelectione.commentaire" label="Commentaire" name="commentaire" />
-                </template>
-
-                <Input v-bind:erreur="appStore.erreursBouteille.quantite" v-model="appStore.bouteilleSelectione.quantite"
-                    label="Quantité" name="quantite" type="number" min="1" />
-
-
-                <Input v-bind:erreur="appStore.erreursBouteille.mellisme" v-model="appStore.bouteilleSelectione.mellisme"
-                    label="Méllisme" name="mellisme" type="number" />
-
-                <Input v-bind:erreur="appStore.erreursBouteille.date_achat"
-                    v-model="appStore.bouteilleSelectione.date_achat" label="Date d'achat" name="date_achat" type="date" />
-
-                <Input v-bind:erreur="appStore.erreursBouteille.garder_jusqu_a"
-                    v-model="appStore.bouteilleSelectione.garder_jusqu_a" label="Garder jusqu'à" name="garder_jusqu_a"
-                    type="date" />
-
-                <Input v-bind:erreur="appStore.erreursBouteille.prix_paye" v-model="appStore.bouteilleSelectione.prix_paye"
-                    label="Prix d'achat" name="prix_paye" type="number" />
-
-                <div class="flex gap-4 justify-between">
-                    <Button texteBouton="Sauvegarder" />
-                    <SecButton texteBouton="Annuler" @click="$emit('cacherFormBouteille')"
-                        class="bg-gray-400 text-gray-900" />
+                    <div
+                        class="mt-2 flex gap-6 w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-rose-800 sm:text-sm sm:leading-6">
+                        <label class="flex gap-2 text-sm leading-6 text-gray-900 ">
+                            <input type="radio" class="form-radio accent-rose-600" name="source" value="saq"
+                                v-model="appStore.bouteilleSelectione.source" checked>
+                            la SAQ
+                        </label>
+                        <label class="flex gap-2 text-sm leading-6 text-gray-900 ">
+                            <input type="radio" class="form-radio accent-rose-600" name="source" value="autre"
+                                v-model="appStore.bouteilleSelectione.source">
+                            Autre
+                        </label>
+                    </div>
                 </div>
 
-            </form>
-        </div>
+                <Input list="suggestions" v-if="(appStore.bouteilleSelectione.source || 'saq') == 'saq'"
+                    v-bind:erreur="appStore.erreursBouteille.bouteille_id" v-model="appStore.bouteilleSelectione.nom"
+                    label="Code saq ou nom bouteille" type="text" autocomplete="off"
+                    @input="appStore.listeSuggestionsBouteilles($event.target.value), afficherSuggestionsBouteilles = true">
+                <template v-slot:liste>
+
+                    <datalist id="suggestions">
+                        <option v-for="bouteille in appStore.suggestionsBouteilles">
+                            {{ bouteille.nom }}
+                        </option>
+                    </datalist>
+
+                </template>
+
+                </Input>
+                <template v-if="appStore.bouteilleSelectione.source == 'autre'">
+
+                    <Input v-bind:erreur="appStore.erreursBouteille.nom" v-model="appStore.bouteilleSelectione.nom"
+                        label="Nom de bouteille" name="nom" type="text" />
+
+                    <Input v-bind:erreur="appStore.erreursBouteille.format" v-model="appStore.bouteilleSelectione.format"
+                        label="Format" name="format" type="text" />
+
+                    <Select v-model="appStore.bouteilleSelectione.type_id" :options="appStore.listeType"
+                        v-bind:erreur="appStore.erreursBouteille.type_id" label="Type" />
+
+                    <Select v-model="appStore.bouteilleSelectione.pays_id" :options="appStore.listePays"
+                        v-bind:erreur="appStore.erreursBouteille.pays_id" label="Pays" />
+                </template>
+            </template>
+
+            <template v-if="appStore.bouteilleSelectione.id">
+
+                <Select v-if="appStore.celliers.length > 1" v-model="appStore.bouteilleSelectione.cellier_id"
+                    :options="appStore.celliers" v-bind:erreur="appStore.erreursBouteille.cellier_id" label="Cellier" />
+
+
+                <Select v-model="appStore.bouteilleSelectione.notes" name="notes" :options="tableauNotes"
+                    v-bind:erreur="appStore.erreursBouteille.notes" label="Notes" />
+
+                <Textarea v-bind:erreur="appStore.erreursBouteille.commentaire"
+                    v-model="appStore.bouteilleSelectione.commentaire" label="Commentaire" name="commentaire" />
+            </template>
+
+            <Input v-bind:erreur="appStore.erreursBouteille.quantite" v-model="appStore.bouteilleSelectione.quantite"
+                label="Quantité" name="quantite" type="number" min="1" />
+
+
+            <Input v-bind:erreur="appStore.erreursBouteille.mellisme" v-model="appStore.bouteilleSelectione.mellisme"
+                label="Méllisme" name="mellisme" type="number" />
+
+            <Input v-bind:erreur="appStore.erreursBouteille.date_achat" v-model="appStore.bouteilleSelectione.date_achat"
+                label="Date d'achat" name="date_achat" type="date" />
+
+            <Input v-bind:erreur="appStore.erreursBouteille.garder_jusqu_a"
+                v-model="appStore.bouteilleSelectione.garder_jusqu_a" label="Garder jusqu'à" name="garder_jusqu_a"
+                type="date" />
+
+            <Input v-bind:erreur="appStore.erreursBouteille.prix_paye" v-model="appStore.bouteilleSelectione.prix_paye"
+                label="Prix d'achat" name="prix_paye" type="number" />
+
+            <div class="flex gap-4 justify-between">
+                <Button texteBouton="Sauvegarder" />
+                <SecButton texteBouton="Annuler" @click="$emit('cacherFormBouteille')" class="bg-gray-400 text-gray-900" />
+            </div>
+
+        </form>
+
     </div>
 </template>
