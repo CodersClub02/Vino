c43bc6d6b783a9e4128faffc58888996715c2cfc
sadektouati <bilal2dellys@hotmail.com>
2023-05-05 12:07:37 -0400

    supprimer le composant entete, ce n'est plus util

--- vue/src/vues/Accueil.vue
+++ vue/src/vues/Accueil.vue
@@ -5,16 +5,16 @@
 import { useAuthStore } from "../stores/auth";
 const authStore = useAuthStore()
 
-import Entete from "../components/Entete.vue";
 import Cellier from "../components/Cellier.vue";
 import Connexion from "../components/Connexion.vue";
 
 </script>
 
 <template>
-  <Entete v-if="authStore.user" titre="Liste des celliers" />
-  <main class="pb-32">
+
+<main class="pb-32">
     <Cellier v-if="authStore.user" />
     <Connexion v-else />
-  </main>
+</main>
+
 </template>
