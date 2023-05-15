305c7e2b1e921eb87b6051203e7a4d6b57524c32
Hanane Boukhalfa <e2195837@cmaisonneuve.qc.ca>
2023-05-15 18:23:06 -0400

    Modifier la couleur du fond de l'animation avant le chargement des données

--- vue/src/components/Chargement.vue
+++ vue/src/components/Chargement.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="absolute inset-0 z-20 bg-gray-400">
+    <div class="absolute inset-0 z-20 bg-black/95">
         <img src="/chargement.gif" alt="chargement" class="w-48 m-auto mt-48">
     </div>
 </template>
